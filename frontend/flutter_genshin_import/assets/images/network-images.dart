92661638f4c50fc85bdc9542773ce6db3e32243c
Nadine Natasja Liu Sugandi <natasjanadine020726@gmail.com>
2026-03-23 16:51:28 +0700

    adding schema

---
+++ frontend/flutter_genshin_import/assets/images/network-images.dart
@@ -0,0 +1,29 @@
+class ArtifactData {
+  String title;
+  String imageUrl;
+
+  ArtifactData(this.title, this.imageUrl);
+}
+
+List<ArtifactData> artifactList = [
+  ArtifactData(
+    'Naruto',
+    'https://static.wikia.nocookie.net/gensin-impact/images/9/90/Item_In_Remembrance_of_Viridescent_Fields.png',
+  ),
+  ArtifactData(
+    'One Piece',
+    'https://static.wikia.nocookie.net/gensin-impact/images/6/65/One_Piece_Volume_1.jpg/revision/latest/scale-to-width-down/60?cb=20201120064508',
+  ),
+  ArtifactData(
+    'Attack on Titan',
+    'https://static.wikia.nocookie.net/gensin-impact/images/7/7a/Shingeki_no_Kyojin_manga_volume_1.jpg/revision/latest/scale-to-width-down/60?cb=20201120064508',
+  ),
+  ArtifactData(
+    'My Hero Academia',
+    'https://static.wikia.nocookie.net/gensin-impact/images/0/0b/My_Hero_Academia_Volume_1.jpg/revision/latest/scale-to-width-down/60?cb=20201120064508',
+  ),
+  ArtifactData(
+    'Demon Slayer',
+    'https://static.wikia.nocookie.net/gensin-impact/images/a/a9/Demon_Slayer_Volume_1.jpg/revision/latest/scale-to-width-down/60?cb=20201120064508',
+  ),
+];
