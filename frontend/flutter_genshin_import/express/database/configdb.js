c2845b304d7d2ad9bf99361f0a9beac16dab49a6
Nadine Natasja Liu Sugandi <natasjanadine020726@gmail.com>
2026-04-29 22:25:40 +0700

    modifying database pool for efficiency

--- frontend/flutter_genshin_import/express/database/configdb.js
+++ frontend/flutter_genshin_import/express/database/configdb.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: 'genshindb', 
+    password: '', // sesuaikan password database kamu
+    database: 'genshin_import', // sesuaikan nama db kamu
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-db.connect();
+module.exports = pool.promise();
