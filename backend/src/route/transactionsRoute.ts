e99a710d70d6242026a60616e07fce6e894a8c08
Nadine Natasja Liu Sugandi <natasjanadine020726@gmail.com>
2026-05-18 21:55:19 +0700

    deleting all backends

--- backend/src/route/transactionsRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { buyItem } from '../controllers/transaction-controllers';
-
-const router = Router();
-
-router.post('/buy', buyItem); 
-
-export default router;
